fd33147c6e24a5b2467284923bb8c30dbe8fe81e
fivedoor <jh5dl@yahoo.co.uk>
2019-03-20 13:23:49 +0000

    Overview: Added basic Bootstrap js and classes for Validation HTML: index.html: Form: Add validation to <form> element with bootstrap classes: class="needs-validation" novalidate HTML: index.html: Form: Add validation to <input> element with bootstrap classes: class="form-control" HTML: index.html: Form: Add validation to email <input> type="email" for validation HTML: index.html: Form: Add validation feedback with class="invalid-feedback" HTML: index.html: Scripts: Adjust scripts hierarchy JS: main.js: add bootstrap4 validation script + HTML: index.html: change main.css to style.css
    Cases to be Resolved:
    1) Validation behaves as expected except in any scenario where radio button selection is valid --> form is submitted and all inputs reset to fail (red)
    2) When form submitted with valid data all inputs reset to fail (red)
    
    Comments:
    Validation appears as expected when id="contactForm" is removed from <form> so unexpected validation results may be due to conflict with main.js: SEND FORM DATA (Firebase) line 78-145

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,86 @@
 console.log('This is the Main.js file. It should be the 3rd and final file');
 
+/* BOOSTRAP4 VALIDATION
+-----------------------*/ 
+
+// Requires class="form-control" added to input
+// Add feedback divs directly after inputs for them to display on submit
+// <div class="invalid-feedback" >Please choose an option</div>
+// <div class="valid-feedback"> Looks good!</div>
+ 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
+
+
+
+/* CUSTOM VALIDATION
+-----------------------*/ 
+
+/*const signUpForm = document.getElementById('contactForm');
+const emailField = document.getElementById('input--email');
+const submitButton = document.getElementById('contactForm__submit');
+const invalidClassName = 'invalid';
+
+emailField.addEventListener('keyup', function (event) {
+  isValidEmail = emailField.checkValidity();
+  
+    if ( isValidEmail ) {
+  emailField.classList.remove(invalidClassName);
+    okButton.disabled = false;
+  } else {
+  emailField.classList.add(invalidClassName);
+    okButton.disabled = true;
+  }
+});
+
+submitButton.addEventListener('click', function (event) {
+  signUpForm.submit();
+});
+*/
+
+
+/*var inputs = document.querySelectorAll('input, select, textarea');
+const invalidClassName = 'invalid';
+
+inputs.forEach(function (input) {
+  // Add a css class on submit when the input is invalid.
+  input.addEventListener('invalid', function () {
+    input.classList.add(invalidClassName);
+  });
+
+  // Remove the class when the input becomes valid.
+  // 'input' will fire each time the user types
+  input.addEventListener('input', function () {
+    if (input.validity.valid) {
+      input.classList.remove(invalidClassName);
+    }
+  });
+})*/
+
+/* SEND FORM DATA (Firebase)
+-----------------------*/ 
+
 // Initialize Firebase (ADD YOUR OWN DATA)
 var config = {
-  apiKey: "xxxxx",
+ apiKey: "xxxxx",
   authDomain: "xxxxx",
   databaseURL: "xxxxx",
   projectId: "xxxxx",
